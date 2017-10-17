41331f5a8c598e6320b85deec748f3fa98a5818f
wangchuang <wangchuang@goldwind.com.cn>
2017-10-17 15:19:14 +0800

    first commit

---
+++ js/test.js
@@ -0,0 +1,17 @@
+
+// $(document).ready(function(){
+//     $("p").click(function(){
+//         $(this).hide();
+//     });
+// });
+
+ $(function () {
+     $('#p1').onclick=function () {
+         alert("1")
+     }
+ })
+
+function a() {
+    alert('111');
+    console.log('1')
+}
